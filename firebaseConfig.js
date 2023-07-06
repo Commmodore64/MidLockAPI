5e8147f800696508e69dbfd3d8d897233629f60b
Cesar Marines <77293672+Commmodore64@users.noreply.github.com>
2023-07-05 20:07:37 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
 // firebaseConfig.js
 
 module.exports = {
-  apiKey: "AIzaSyBdMl48n0Dea2z2VUkZB0b-vLd1HoGH2MM",
-  authDomain: "midlock-5bcc3.firebaseapp.com",
-  databaseURL: "https://midlock-5bcc3-default-rtdb.firebaseio.com",
-  projectId: "midlock-5bcc3",
-  storageBucket: "midlock-5bcc3.appspot.com",
-  messagingSenderId: "835789461317",
-  appId: "1:835789461317:web:7f65f966ad2cca2954d02c",
+  apiKey: "xxx",
+  authDomain: "xxx",
+  databaseURL: "xxx,
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx",
 };
